06483ea921ffe32e09ca69c0bee5e5adfd287c78
Drew Sartorius <duckydesign@gmail.com>
2019-04-20 22:55:00 -0400

    Testing with hero and header

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,26 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from 'styled-components';
+
+const Hero = styled.section`
+  background: url('../images/hero.jpg');
+
+`;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <Hero>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+    </Hero>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </Layout >
 )
 
 export default IndexPage
