1c40625b7486fc418e8f495124d96abc2b614173
Drew Sartorius <duckydesign@gmail.com>
2019-08-13 23:57:13 -0400

    Header logo updated and animated

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,42 @@ import styled from 'styled-components';
 
 const HeaderContainer = styled.header`
   background: #37474F;
-  padding: 1rem 0.5rem;
+  padding: 0rem;
   color: #fff;
+`;
+
+const HeaderLogo = styled.h1`
+  font-size: 3rem;
+  border: solid 3px white;
+  padding: 0.1rem 0.5rem 0.5rem 0.3rem;
+  width: 4.2rem;
+  height: 4.2rem;
+  position: relative;
+  background: linear-gradient(to top left, #fff, #fff, #33333300);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-position: 0 0;
+  transition: 0.5s ease-out all;
+
+  span {
+    position: absolute;
+    right: 0.4rem;
+    bottom: 0rem;
+    background: linear-gradient(to bottom right, #fff, #fff, #33333300);
+    background-position: 0 0;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    transition: 0.5s ease-out all;
+  }
+
+  &:hover {
+    background-position: -60px -60px;
+    border-color: #ffffff99;
+
+    span {
+      background-position: 32px 49px;
+    }
+  }
 `;
 
 
@@ -19,17 +53,15 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <HeaderLogo style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
             color: `white`,
             textDecoration: `none`,
           }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+        >D<span>S</span></Link>
+      </HeaderLogo>
     </div>
   </HeaderContainer>
 )
